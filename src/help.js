751c2c6020f53416eae1f8404c78665879dfa22c
Pickbot <alphalgd@gmail.com>
2021-03-07 09:49:50 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,128 @@
+const help = (p, date) => {
+return `饾暰饾枈饾枏饾枂饾枓 饾暛饾枈饾枓-饾杹饾枎饾枔饾枆饾枖饾枠 饾枂饾枖 饾暩饾枈饾枔饾枤 饾枆饾枖
+馃拵 鈻佲杺鈻冟蓟饾暛饾枟饾枎饾枱饾枂饾枠-饾枃饾枖饾枡 2.0嗉衡杻鈻傗杹 馃拵
+
+饾暫 饾枍饾枖饾枟谩饾枟饾枎饾枖 饾枂饾枡饾枤饾枂饾枒 茅:
+*${date}*
+
+鈺斺晲鈺愨晲鈺� 鉂� 饾摗饾摦饾摥饾摦饾摷 饾摷饾摳饾摤饾摬饾摢饾摬饾摷 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� insta: Hoshigaki_Guilherme
+鈺� 鉂� Zap: Wa.me//+5519999897566
+鈺�
+鈺� !岽徥欔湵 岽勧磤隃贬磸 岽囮湵隃贬磭 纱岽溼磵岽囀�岽� 隃贬磭岽娽磤 蕶岽�纱瑟岽呩磸 
+鈺� 岽犪磭岽娽磤 岽�隃� 蕗岽囜磪岽囮湵 隃贬磸岽勆磤瑟隃� 岽呩磸 蕶岽忈礇 岽樶磤蕗岽� 
+鈺� 岽犪磭蕗 岽� 纱岽忈礌岽� 纱岽溼磵岽囀�岽�!
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摴饾摶饾摬饾摲饾摤饾摬饾摴饾摢饾摬饾摷 鉂� 鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *${p}destrava*
+鈺� 鉂� *${p}ping*
+鈺� 鉂� *${p}trava* <qnt> <num>
+鈺� 鉂� *${p}criador*
+鈺� 鉂� *${p}botvip*
+鈺� 鉂� *${p}info*
+鈺� 鉂� *${p}blocklist*
+鈺� 鉂� *${p}toimg* <caption, reply sticker>
+鈺� 鉂� *${p}blacklist*
+鈺� 鉂� *${p}wallpaper*
+鈺� 鉂� *${p}tomp3* <reply video>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摷饾摻饾摬饾摤饾摯饾摦饾摶 鉂� 鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *${p}stiker* <caption, reply image>
+鈺� 鉂� *${p}sticker* <caption, reply image>
+鈺� 鉂� *${p}fstiker* <caption, reply image>
+鈺� 鉂� *${p}fsticker* <caption, reply image>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摱饾摳饾摶饾摷饾摦 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺�
+鈺� 鉂� *${p}txtomorse* <text>
+鈺� 鉂� *${p}morsetotxt* <text>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摴饾摶饾摦饾摽饾摬饾摷饾摢饾摳 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺�
+鈺� 鉂� *${p}ship* <num1> <num2>
+鈺� 鉂� *${p}corno* <num1>
+鈺� 鉂� *${p}randomship* <num1 or nothin>
+鈺� 鉂� *${p}gado* <num1>
+鈺� 鉂� *${p}gostosa* <num1>
+鈺� 鉂� *${p}gostoso* <num1>
+鈺� 鉂� *${p}gay* <num1>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摥饾摦饾摷饾摣饾摢饾摲饾摬饾摶 饾摲饾摼饾摱饾摦饾摶饾摳 饾摥饾摳 饾攢饾摴饾摴 鉂� 鈺愨晲鈺�
+鈺� 鉂� *${p}unbanwpp* <num>
+鈺� 岽噚: +55 71 98764-5787
+鈺�
+鈺� 岽徥欔湵: 纱岽�岽� 岽� 散岽�蕗岽�纱岽浬磪岽� 岽� 岽∈溼磤岽涥湵岽�岽樶礃
+鈺� 岽呩磭隃笔欋磤纱瑟蕗 岽� 纱岽溼磵岽囀�岽� 岽� 岽呩磭岽嶀磸蕗岽� 岽勧磭蕗岽勧磤 岽呩磭
+鈺� 2 岽� 7 蕼蕗隃� 岽樶磤蕗岽� 岽� 岽∈溼磤岽涥湵岽�岽樶礃 岽呩磭隃笔欋磤纱瑟蕗
+鈺� 隃搬磤岽犪磸蕗 纱岽�岽� 隃笆熱磸岽忈磪岽�蕗, 隃贬磭散岽溼磭 岽� 岽噚岽囜磵岽樖熱磸 岽嚿瘁礇岽�岽�
+鈺� 岽�岽勆磵岽� 岽樶磤蕗岽� 岽樶磸蕗 岽� 纱岽溼磵岽囀�岽� 纱岽� 岽徥�岽呩磭岽� 岽勧磭蕗岽涐磤,
+鈺� 隃贬磸 隃搬礈纱岽勆磸纱岽� 纱岽溼磵岽囀�岽応湵 蕶蕗岽�隃鄙熱磭瑟蕗岽応湵
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摵饾摶饾摤饾摳饾摥饾摦 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺�
+鈺� 鉂� *${p}qrcode* <text>
+鈺� 鉂� *${p}qrcodegb* <text>
+鈺� 鉂� *${p}qrcodebb* <text>
+鈺� 鉂� *${p}qrcoderb* <text>
+鈺� 鉂� *${p}qrcodebg* <text>
+鈺� 鉂� *${p}qrcodebb1* <text>
+鈺� 鉂� *${p}qrcodebr* <text>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摻饾摦饾攣饾摻饾摳 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺�
+鈺� 鉂� *${p}cep* <cep>
+鈺� 鉂� *${p}cpf*
+鈺� 鉂� *${p}ip* <ip>
+鈺� 鉂� *${p}ddd* <ddd>
+鈺� 鉂� *${p}textcmd*
+鈺� 鉂� *${p}moddroid* <app name>
+鈺� 鉂� *${p}wame*
+鈺� 鉂� *${p}repeat* <text>
+鈺� 鉂� *${p}ocr* <caption, reply sticker>
+鈺� 鉂� *${p}tiktokstalk* <tik tok name>
+鈺� 鉂� *${p}nethunter*
+鈺� 鉂� *${p}signome* <name>
+鈺� 鉂� *${p}idioma*
+鈺� 鉂� *${p}frase*
+鈺� 鉂� *${p}destrava*
+鈺� 鉂� *${p}wppim*
+鈺� 鉂� *${p}db*
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摱饾摦饾摱饾摦 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *${p}meme*
+鈺� 鉂� *${p}memeindo*
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摢饾摼饾摥饾摬饾摳 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *${p}gtts* <la> <text>
+鈺� 鉂� *${p}playlist*
+鈺� 鉂� *${p}ytsearch* <video name youtube>
+鈺� 鉂� *${p}ytmp4* <link youtube>
+鈺� 鉂� *${p}ytmp3* <link youtube>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摢饾摲饾摬饾摱饾摦 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺�
+鈺� 鉂� *${p}randomanime*
+鈺� 鉂� *${p}randomshota*
+鈺� 鉂� *${p}randomkiss*
+鈺� 鉂� *${p}randomhug*
+鈺� 鉂� *${p}randomcry*
+鈺� 鉂� *${p}nekoanime*
+鈺� 鉂� *${p}wait* <caption, reply image>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摴饾摳饾摶饾摲饾摳 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *${p}packmega18*
+鈺� 鉂� *${p}randomhentai*
+鈺� 鉂� *${p}nsfwblowjob*
+鈺� 鉂� *${p}nsfwneko*
+鈺� 鉂� *${p}nsfwtrap*
+鈺� 鉂� *${p}nsfw* <mode 1 or 0>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摪饾摶饾摼饾摴饾摳 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *${p}welcome* <mode 1 or 0>
+鈺� 鉂� *${p}linkgroup*
+鈺� 鉂� *${p}add* <number>
+鈺� 鉂� *${p}listadmin* 
+鈺� 鉂� *${p}listonline* 
+鈺� 鉂� *${p}kick* <dial number>
+鈺� 鉂� *${p}leave*
+鈺� 鉂� *${p}promote* <dial number>
+鈺� 鉂� *${p}demote* <dial number>
+鈺� 鉂� *${p}simih* <mode 1 or 0>
+鈺� 鉂� *${p}tagall*
+鈺� 鉂� *${p}tagall2*
+鈺� 鉂� *${p}tagall3*
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摤饾摱饾摥 饾摥饾摦 饾摴饾摶饾摳饾摴饾摬饾摦饾摻饾摢饾摶饾摬饾摳 鉂� 鈺愨晲鈺�
+鈺� 鉂� *${p}bc* <text>
+鈺� 鉂� *${p}block* <text>
+鈺� 鉂� *${p}unblock* <text>
+鈺犫晲鈺愨晲鈺愨晲 鉂� 饾摪饾摬饾摻饾摫饾摼饾摣 鉂� 鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲
+鈺� 鉂� *https://enrt.eu/GZwCeqNb*
+鈺氣晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺愨晲鈺恅
+}
+
+exports.help = help
+
+
